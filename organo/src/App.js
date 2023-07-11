3a0252f030739ed299000acaa591955cbcef763b
fox <fernandodiego.sn@gmail.com>
2023-07-11 19:51:48 -0300

    Limpando o projeto. Curso Alura de React

--- organo/src/App.js
+++ organo/src/App.js
@@ -1,5 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
 import Banner from './components/Banner/Banner';
 
 
@@ -7,21 +5,6 @@ function App() {
   return (
     <div className="App">
         <Banner></Banner>
-        <h1>Olá mundo</h1>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Hello World!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
