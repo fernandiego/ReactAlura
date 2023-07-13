cc758c55b1450f43063b5f3b7146add2c5afe6b6
fox <fernandodiego.sn@gmail.com>
2023-07-13 08:31:04 -0300

    Criado componente Formulário e implementando um css. Curso Alura de React

--- organo/src/components/Formulario/index.js
+++ organo/src/components/Formulario/index.js
@@ -1,10 +1,12 @@
 import CampoTexto from "../CampoTexto";
+import './Formulario.css'
 
 
 const Formulario = () => {
     return (
-        <section>
+        <section className="formulario">
             <form>
+                <h2>Preencha os dados para criar os cards</h2>
                 <CampoTexto label="Nome" placeholder="Digite seu nome"></CampoTexto>
                 <CampoTexto label="Cargo" placeholder="Digite seu cargo"></CampoTexto>
                 <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"></CampoTexto>
