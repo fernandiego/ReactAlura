78994a7c8cfa13bc1b0ab226d34c81ca1e8c9c56
fox <fernandodiego.sn@gmail.com>
2023-07-15 20:26:31 -0300

    Style das seções cards. Curso Alura de React

--- organo/src/components/Time/index.js
+++ organo/src/components/Time/index.js
@@ -2,8 +2,8 @@ import './Time.css'
 
 const Time = (props) => {
 return (
-    <section className="time">
-        <h3>
+    <section className="time" style={{backgroundColor: props.corSecundaria}}>
+        <h3 style={{borderColor: props.corPrimaria}}>
             {props.nome}
         </h3>
     </section>
