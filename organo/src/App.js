ebb1945deb05246c51217554435edda970b1ce20
fox <fernandodiego.sn@gmail.com>
2023-07-15 16:26:55 -0300

    Estrutura inicial de cards. Curso Alura de React

--- organo/src/App.js
+++ organo/src/App.js
@@ -1,6 +1,7 @@
 import Banner from './components/Banner';
 import Formulario from "./components/Formulario";
 import {useState} from "react";
+import Time from "./components/Time";
 
 
 function App() {
@@ -11,13 +12,19 @@ function App() {
         setColaboradores([...colaboradores, colaborador])
     }
 
-  return (
-    <div className="App">
-        <Banner></Banner>
-        <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}></Formulario>
-
-    </div>
-  );
+    return (
+        <div className="App">
+            <Banner></Banner>
+            <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}></Formulario>
+            <Time nome="Programação"></Time>
+            <Time nome="Front-Ent"></Time>
+            <Time nome="Data Science"></Time>
+            <Time nome="Devops"></Time>
+            <Time nome="UX e Design"></Time>
+            <Time nome="Mobile"></Time>
+            <Time nome="Inovação e Gestão"></Time>
+        </div>
+    );
 }
 
 export default App;
---
+++ organo/src/components/Time/index.js
@@ -0,0 +1,13 @@
+import './Time.css'
+
+const Time = (props) => {
+return (
+    <section className="time">
+        <h3>
+            {props.nome}
+        </h3>
+    </section>
+)
+}
+
+export default Time
